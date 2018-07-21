f1796908bd1c118f52bd4e939ee2f1940531e0d2
stenoritama <shr.nrht.0623@gmail.com>
2018-07-21 21:54:36 +0900

    wip

--- src/main/scala/com/github/stenoritama/chap04/Option.scala
+++ src/main/scala/com/github/stenoritama/chap04/Option.scala
@@ -4,8 +4,10 @@ sealed trait Option[+A] {
   def map[B](f: A => B): Option[B]
   def flatMap[B](f: A => Option[B]): Option[B]
   def getOrElse[B >: A](default: => B): B
-  def 
+  def orElse[B >: A](ob: => Option[B]): Option[B]
+  def filter(f: A => Boolean): Option[A]
 }
 
-case class Some[+A](get: A) extends Option[A]
-case object None extends Option[Nothing]
+trait Helper {
+  def lift[A, B](f: A => B): Option[A] => Option[B] = _ map f
+}
